832f10737f406f62a5d03203aebbfd160dc68db2
Ivana Tosheva Aleksoska <ivana.toshevaaleksoska@edu.fh-joanneum.at>
2026-01-15 01:23:32 +0100

    Set background closes #24

--- src/hangman_graphic.py
+++ src/hangman_graphic.py
@@ -10,4 +10,17 @@ pygame.display.set_caption("Hangman GUI")
 clock = pygame.time.Clock()
 FPS = 60
 
+# Set background image
+background = pygame.image.load("../assets/images/blackboard.jpg").convert()
+background = pygame.transform.scale(background, (WIDTH, HEIGHT))
+
+running = True
+while running:    # Main loop
+    clock.tick(FPS)
+    
+    # Draw background
+    screen.blit(background, (0,0))
+    # Show to everyone
+    pygame.display.update()
+
 pygame.quit()
