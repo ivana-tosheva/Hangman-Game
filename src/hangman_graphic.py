5684b1f1b9bd4910ecbeb2c8a2004ba31248a0ba
Ivana Tosheva Aleksoska <ivana.toshevaaleksoska@edu.fh-joanneum.at>
2026-01-15 02:24:01 +0100

    Placed gallows closes #25

--- src/hangman_graphic.py
+++ src/hangman_graphic.py
@@ -14,12 +14,21 @@ FPS = 60
 background = pygame.image.load("../assets/images/blackboard.jpg").convert()
 background = pygame.transform.scale(background, (WIDTH, HEIGHT))
 
+# Set gallows
+G_WIDTH = 450
+G_HEIGHT = 400
+gallows_position = (100, 50)
+
+gallows = pygame.image.load("../assets/images/gallows.png").convert_alpha()
+gallows = pygame.transform.scale(gallows, (G_WIDTH, G_HEIGHT))
+
 running = True
 while running:    # Main loop
     clock.tick(FPS)
     
     # Draw background
     screen.blit(background, (0,0))
+    screen.blit(gallows, gallows_position)
     # Show to everyone
     pygame.display.update()
 
