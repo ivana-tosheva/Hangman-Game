b77f08943039b0a68aa3f9aac8738816d218bdb5
Ivana Tosheva Aleksoska <ivana.toshevaaleksoska@edu.fh-joanneum.at>
2026-01-17 12:48:00 +0100

    Add categories, word from random category and event handler. Closes #27, #28

--- src/hangman_graphic.py
+++ src/hangman_graphic.py
@@ -1,5 +1,6 @@
 import pygame
 import string
+import random
 
 
 # Window
@@ -29,10 +30,46 @@ gallows = pygame.transform.scale(gallows, (G_WIDTH, G_HEIGHT))
 
 #font
 FONT = pygame.font.SysFont("Arial", 45)
-GOLD = (252, 214, 135)
-WOOD_BROWN = (109, 59, 29)
+#colors
+GOLD = (222, 184, 135)
+WOOD_BROWN = (139, 69, 19)
+LIGHT_BROWN = (180, 120, 60)
+GRAY = (122,84,35)
 
-#functions
+
+def load_file_content(filename: str) -> list[str]:
+    with open(filename) as file:
+        return file.read().split(", ")
+
+categories = {
+    "E": load_file_content("emotions.txt"),
+    "A": load_file_content("animals.txt"),
+    "F": load_file_content("food.txt"),
+    "S": load_file_content("science.txt"),
+    "H": load_file_content("hobby.txt")
+}
+
+chosen_category = random.choice(list(categories.keys()))
+word = random.choice(categories[chosen_category]).upper()
+
+guessed_letters = []
+wrong_letters = []
+guesses_left = 6
+game_over = False
+win = False
+
+
+def get_display_word(word, guessed):
+    return " ".join(letter if letter in guessed else "_" for letter in word)
+
+
+def draw_word(screen, word, guessed):
+    text = FONT.render(get_display_word(word, guessed), True, GOLD)
+    text_rect = text.get_rect(center=(WIDTH // 2, 350))
+    screen.blit(text, text_rect)
+
+
+# LETTER BUTTONS
 def create_letter_buttons(start_x, start_y):
     buttons = []
     letters_per_row = 13
@@ -40,43 +77,86 @@ def create_letter_buttons(start_x, start_y):
     for index, letter in enumerate(string.ascii_uppercase):
         row = index // letters_per_row  # choses between row0 and row1(14//13=1)
         column = index % letters_per_row  # sets the column number (1%13=1, 5%13=5)
+
         x = start_x + column * (BUTTON_SIZE + BUTTON_SPACING)
         y = start_y + row * (BUTTON_SIZE + BUTTON_SPACING)
         
         buttons.append({
             "rect": pygame.Rect(x, y, BUTTON_SIZE, BUTTON_SIZE),
-            "letter": letter
+            "letter": letter,
+            "active": True
             })
     return buttons
 
 
-def draw_letter_buttons(screen, buttons, font):
+def draw_letter_buttons(screen, buttons):
     for button in buttons:
-        # Button background
-        pygame.draw.rect(screen, WOOD_BROWN, button["rect"], border_radius=6)
-        # Border
-        pygame.draw.rect(screen, GOLD, button["rect"], width=2, border_radius=6)
-        # Draw letter
-        text = font.render(button["letter"], True, GOLD)
-        text_rect = text.get_rect(center=button["rect"].center)
-        # Draw to screen
-        screen.blit(text, text_rect)
+        if button["active"]:
+            bg_color = WOOD_BROWN
+            border_color = GOLD
+            letter_color = GOLD
+        else:
+            bg_color = LIGHT_BROWN
+            border_color = GRAY
+            letter_color = GRAY
+            
+        
+        # Draw button background
+        pygame.draw.rect(screen, bg_color, button["rect"], border_radius=6)
+        # Draw button border
+        pygame.draw.rect(screen, border_color, button["rect"], width=5, border_radius=6)
+        # Draw the letter
+        letter = FONT.render(button["letter"], True, letter_color)
+        letter_rect = letter.get_rect(center=button["rect"].center)
+        screen.blit(letter, letter_rect)
 
 
-#setup
+def handle_button_click(mouse_pos, buttons):
+    for button in buttons:
+        if button["active"] and button["rect"].collidepoint(mouse_pos):
+            button["active"] = False  # deactivate permanently
+            return button["letter"]     # can be used for Hangman logic
+    return None
+
+#SETUP
 alphabet_buttons = create_letter_buttons(70, 435)
 running = True
 
-#loop
-while running:    # Main loop
+#Main loop
+while running:
     clock.tick(FPS)
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = pygame.mouse.get_pos()
+            letter = handle_button_click(mouse_pos, alphabet_buttons)
+            
+            if letter:
+                #print("Clicked:", letter)  # placeholder for game logic
+                if letter in word:
+                    guessed_letters.append(letter)
+                else:
+                    wrong_letters.append(letter)
+                    guesses_left -=1
+                    
+                if all(l in guessed_letters for l in word):
+                    game_over = True
+                    win = True
+                
+                if guesses_left <= 0:
+                    game_over = True
+    
+    
     
     # Draw background
     screen.blit(background, (0,0))
     screen.blit(gallows, gallows_position)
-
+    #Draw word
+    draw_word(screen, word, guessed_letters)
     # Draw letter buttons
-    draw_letter_buttons(screen, alphabet_buttons, FONT)
+    draw_letter_buttons(screen, alphabet_buttons)
 
     # Update display (show)
     pygame.display.update()
